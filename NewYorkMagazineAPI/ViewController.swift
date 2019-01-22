e739aa237e8e937d35d3ac60293d8c05c4248137
Hollie <fangsihan@sihande-macbook-air.local>
2019-01-22 15:31:34 +0800

    Initial Commit

---
+++ NewYorkMagazineAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewYorkMagazineAPI
+//
+//  Created by Sihan Fang on 2019/1/22.
+//  Copyright © 2019 Sihan Fang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
