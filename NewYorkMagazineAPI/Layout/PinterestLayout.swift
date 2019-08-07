c00357e4d1aef0790e79415801e5b495514fbeb7
shya008 <w0465911@gmail.com>
2019-08-07 12:48:39 +0800

    UI updated

---
+++ NewYorkMagazineAPI/Layout/PinterestLayout.swift
@@ -0,0 +1,13 @@
+//
+//  Layout.swift
+//  NewYorkMagazineAPI
+//
+//  Created by Sihan Fang on 2019/7/30.
+//  Copyright © 2019 Sihan Fang. All rights reserved.
+//
+
+import UIKit
+
+class Layout: UICollectionViewLayout {
+
+}
